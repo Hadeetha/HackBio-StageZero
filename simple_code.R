6597721a017e04a45799db10c905c7fb605a80cb
Shuaib Haditha <haditha16@gmail.com>
2026-01-23 18:37:24 +0100

    modified simple_code.R

--- simple_code.R
+++ simple_code.R
@@ -1,14 +1,10 @@
 # Define parameters 
 name <- "Haditha Shuaib"
-profession <- "molecular microbiologist"
-origin <- "Nigeria"
+affiliation <- "molecular microbiologist"
 fav_gene <- "E1"
 virus <- "Chikungunya virus (CHIKV)"
 
 # Use paste0 to construct the sentence
-introduction <- paste0("Hi, my name is ", name, ", I am a ", profession, 
-                       " from ", origin, ". My favourite gene is ", 
-                       fav_gene, " in ", virus, ".")
-
-# Print the result
-cat(introduction)
+print(paste0("Hi, my name is ", name, ", I am a ", affiliation, 
+                       " from Nigeria ",  ". My favourite gene is ", 
+                       fav_gene, " in ", virus, "."))
